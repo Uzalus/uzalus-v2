1517f00ffb0a0eb250a1a0a04bde20d92ba3329b
Z User <z@container>
2026-08-15 14:55:32 +0000

    427a1d6b-b8a7-42d2-9d0e-2790c962ad03

--- src/components/uzalus/navbar.tsx
+++ src/components/uzalus/navbar.tsx
@@ -45,8 +45,9 @@ import {
 const navLinks = [
   { key: 'nav.home', href: '/' },
   { key: 'nav.shop', href: '/categories', megaMenu: true },
-  { key: 'nav.categories', href: '#categories' },
+  { key: 'nav.categories', href: '/categories' },
   { key: 'nav.deals', href: '#promotions' },
+  { key: 'nav.tools', href: '/tools' },
 ];
 
 const shopCats = [
